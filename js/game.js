4a5c1b7e92f92b219eaabe7c4ff97813fadf0d49
Alex Kaufman <alexkaufman06@gmail.com>
2016-11-03 08:34:45 -0700

    Move logic into react and update user interface

--- js/game.js
+++ js/game.js
@@ -1,24 +1,169 @@
+var player = { money: 100, score: 0 };
+var gameStatus = 1;
+var deck = [];
+var hand = [];
+
+function Card (rank, suit) {
+  this.rank = rank;
+  this.suit = suit;
+}
+
+function sortByRank(a,b) {
+  return a - b;
+}
+
 var Poker = React.createClass({
+  getInitialState: function() {
+    return {gameOver: false};
+  },
+
+  makeDeck: function() {
+    var ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+    // 1=Ace; 11=Jack; 12=Queen; 13=King
+    var suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+
+    for (let s = 0; s < suits.length; s++) {
+      for (let r = 0; r < ranks.length; r++) {
+        deck.push(new Card(ranks[r], suits[s]));
+      }
+    }
+  },
+
+  shuffle: function() {
+    for (let i = deck.length; i; i--) {
+      let j = Math.floor(Math.random() * i);
+      [deck[i-1], deck[j]] = [deck[j], deck[i-1]];
+    }
+  },
+
+  makeHand: function() {
+    for (let i=0; i < 5; i++) {
+      hand.push(deck[i]);
+    }
+  },
+
+  rankHand: function() {
+    var handRanks = [];
+
+    for (let i=0; i < 5; i++) {
+      handRanks.push(parseInt(hand[i].rank));
+    }
+
+    handRanks.sort(sortByRank);
+
+    if ( handRanks[0] == 1 &&
+         handRanks[1] == 10 &&
+         handRanks[2] == 11 &&
+         handRanks[3] == 12 &&
+         handRanks[4] == 13 &&
+         hand[0].suit == hand[1].suit &&
+         hand[1].suit == hand[2].suit &&
+         hand[2].suit == hand[3].suit &&
+         hand[3].suit == hand[4].suit
+       ) {
+      return "Royal Flush";
+    } if ( handRanks[0] + 1 == handRanks[1] &&
+           handRanks[1] + 1 == handRanks[2] &&
+           handRanks[2] + 1 == handRanks[3] &&
+           handRanks[3] + 1 == handRanks[4] &&
+           hand[0].suit == hand[1].suit &&
+           hand[1].suit == hand[2].suit &&
+           hand[2].suit == hand[3].suit &&
+           hand[3].suit == hand[4].suit
+         ) {
+      return "Straight Flush";
+    } if ( handRanks[0] == handRanks[1] &&
+           handRanks[1] == handRanks[2] &&
+           handRanks[2] == handRanks[3] ||
+           handRanks[1] == handRanks[2] &&
+           handRanks[2] == handRanks[3] &&
+           handRanks[3] == handRanks[4]
+         ) {
+      return "Four of a Kind";
+    } if ( handRanks[0] == handRanks[1] &&
+           handRanks[1] == handRanks[2] &&
+           handRanks[3] == handRanks[4] ||
+           handRanks[0] == handRanks[1] &&
+           handRanks[2] == handRanks[3] &&
+           handRanks[3] == handRanks[4] 
+         ) {
+      return "Full House";
+    } if ( hand[0].suit == hand[1].suit &&
+           hand[1].suit == hand[2].suit &&
+           hand[2].suit == hand[3].suit &&
+           hand[3].suit == hand[4].suit
+         ) {
+      return "Flush";
+    } if ( handRanks[0] + 1 == handRanks[1] &&
+           handRanks[1] + 1 == handRanks[2] &&
+           handRanks[2] + 1 == handRanks[3] &&
+           handRanks[3] + 1 == handRanks[4] ||
+           handRanks[0] == 1 &&
+           handRanks[1] == 10 &&
+           handRanks[2] == 11 &&
+           handRanks[3] == 12 &&
+           handRanks[4] == 13
+         ) {
+      return "Straight";
+    } if ( handRanks[0] == handRanks[1] &&
+           handRanks[1] == handRanks[2] ||
+           handRanks[1] == handRanks[2] &&
+           handRanks[2] == handRanks[3] ||
+           handRanks[2] == handRanks[3] &&
+           handRanks[3] == handRanks[4] 
+         ) {
+      return "Three of a Kind";
+    } if ( handRanks[0] == handRanks[1] &&
+           handRanks[2] == handRanks[3] ||
+           handRanks[1] == handRanks[2] &&
+           handRanks[3] ==handRanks[4] 
+         ) {
+      return "Two Pair";
+    } if ( handRanks[0] == handRanks[1] ||
+           handRanks[1] == handRanks[2] || 
+           handRanks[2] == handRanks[3] || 
+           handRanks[3] == handRanks[4] 
+          ) {
+      return "Pair";
+    } else {
+      return "No Hand";
+    }
+  },
+
+  newDeal: function() {
+    deck = [];
+    hand = [];
+    this.makeDeck();
+    this.shuffle();
+    this.makeHand();
+  },
+
   render: function() {
+    this.makeDeck();
+    this.shuffle();
+    this.makeHand();
+
     return <div>
       <h1 className="text-center">Poker</h1>
       <div className="row text-center">
       	<div className="col-md-2 col-md-offset-1">
-      		<p>CARD &spades;</p>
+      		<p>{hand[0].rank} {hand[0].suit}</p>
       	</div>
       	<div className="col-md-2">
-      		<p>CARD &hearts;</p>
+      		<p>{hand[1].rank} {hand[1].suit}</p>
       	</div>
       	<div className="col-md-2">
-      		<p>CARD &diams;</p>
+      		<p>{hand[2].rank} {hand[2].suit}</p>
       	</div>
       	<div className="col-md-2">
-      		<p>CARD &clubs;</p>
+      		<p>{hand[3].rank} {hand[3].suit}</p>
       	</div>
       	<div className="col-md-2">
-      		<p>CARD &diams;</p>
+      		<p>{hand[4].rank} {hand[4].suit}</p>
       	</div>
       </div>
+      <hr/>
+      <h3 className="text-center">{this.rankHand()}</h3>
     </div>
   }
 });
